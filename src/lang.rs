000f562fc192cbde63f677a156ce8f9ab9986f8c
Coleton O'Donnell <contact@coleton.io>
2023-09-15 15:13:48 -0400

    Move lang.rs into proper directory (src)

--- src/lang.rs
+++ src/lang.rs
@@ -40,6 +40,7 @@ pub fn lang_for_file(file_name: &str) -> &'static LanguageConfig {
     match file_name.split('.').last() {
         Some("py") => &PYTHON_LANGUAGE,
         Some("java") => &JAVA_LANGUAGE,
+        Some("cs") => &CS_LANGUAGE,
         _ => &PYTHON_LANGUAGE, // TODO: plain text mode?
     }
 }
@@ -103,6 +104,61 @@ const JAVA_LANGUAGE: LanguageConfig = LanguageConfig {
                     Some(If)
                 }
             }
+            "for_statement" => Some(For),
+            "try_statement" => Some(Try),
+
+            // normal expressions (incomplete)
+            "import_declaration" => Some(Generic),
+            "expression_statement" => Some(Generic),
+            "local_variable_declaration" => {
+                // don't create a block for a for loop's variable declaration
+                if node.parent().map_or("", |p| p.kind()) == "for_statement" {
+                    None
+                } else {
+                    Some(Generic)
+                }
+            }
+            "field_declaration" => Some(Generic),
+            "return_statement" => Some(Generic),
+            "assert_statement" => Some(Generic),
+            "line_comment" => Some(Generic),
+            "block_comment" => Some(Generic),
+
+            // dividers to keep generics from merging
+            "block" => Some(Divider),
+
+            // do not handle the rest
+            _ => None,
+        }
+    },
+    string_node_ids: StringNodeIDs {
+        string: 141,
+        string_bounds: &[11, 12], // 11 is single quote, 12 is double quote
+    },
+};
+
+const CS_LANGUAGE: LanguageConfig = LanguageConfig {
+    name: "c#",
+    ts_lang: tree_sitter_c_sharp::language,
+    highlight_query: tree_sitter_c_sharp::HIGHLIGHT_QUERY,
+    new_scope_char: '{',
+    node_categorizer: |node| {
+        use BlockType::*;
+
+        match node.kind() {
+            // scopes
+            "class_declaration" => Some(Class),
+            "method_declaration" => Some(FunctionDef),
+            "while_statement" => Some(While),
+            "if_statement" => Some(If),//{
+                // the java grammar treats else if as else, if_statement
+                // so check that is isn't that
+                //if node.prev_sibling().map_or("", |s| s.kind()) == "else" {
+                  //  None
+              //  } else {
+               //     Some(If)
+               // }
+            //}
             "for_statement" => Some(For),
             "try_statement" => Some(Try),
 
